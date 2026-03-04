ff723ca390ab839f916c216e7e237f9925fb15a7
oliviaaa10 <oliviapolus10@gmail.com>
2026-03-04 12:44:52 +0800

    update

--- labs/lab08/exercise2/exercise2.py
+++ labs/lab08/exercise2/exercise2.py
@@ -2,21 +2,29 @@
 # Write your code below:
 
 def merge_lists(file1, file2, output_file):
-    """
-    Merge two lists of names, remove duplicates, and sort.
 
-    Args:
-        file1: path to first list file
-        file2: path to second list file
-        output_file: path to output file
+    f1 = open(file1, "r")
+    name1 = set(f1.readlines())
+    f1.close()
 
-    Returns:
-        int: count of unique names
-    """
-    # TODO: Implement this function
-    pass
+    f2 = open(file2, "r")
+    name2 = set(f2.readlines())
+    f2.close()
+
+    unique_names = name1 | name2
+
+    sorted_names = sorted(unique_names)
+
+
+    f_out = open(output_file, "w")
+    for name in sorted_names :
+        f_out.write(name)
+    f_out.close()
+    
+
+    return len(sorted_names)
 
 
 # Test your code here
-result = merge_lists("data/list1.txt", "data/list2.txt", "data/merged.txt")
+result = merge_lists("labs/lab08/exercise2/data/list1.txt", "labs/lab08/eercise2/data/list2.txt", "data/merged.txt")
 print(f"Unique names: {result}")
--- labs/lab08/sandbox.py
+++ labs/lab08/sandbox.py
@@ -6,6 +6,15 @@ scores.append(85)
 scores.append(92)
 scores.append(78)
 
-print("Scores:", scores)
+print(scores)
 
 # Try running this program again - scores will be empty!
+
+# Write scores to a file
+f = open("data/scores.txt", "w")
+f.write("85\n")
+f.write("92\n")
+f.write("78\n")
+f.close()
+
+print("Scores saved to file!")
